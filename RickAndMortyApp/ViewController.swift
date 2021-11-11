5dd74c872d7ed385a1a7a47872d6b8954fba6bd4
Vinicius Teixeira <vinicius.teixeira@Viniciuss-MacBook-Air.local>
2021-11-11 20:16:49 -0300

    first commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Vinicius Teixeira on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
